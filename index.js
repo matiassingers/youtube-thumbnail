c7e13fe3c6977f9dc1a35b9598420d25b0dbf250
Matias Singers <mail@matiassingers.com>
2015-02-02 16:15:13 +0800

    Add initial logic to index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var getYouTubeID = require('get-youtube-id');
+
+module.exports = function(url){
+  var id = getYouTubeID(url);
+
+  return {
+    default: {
+      url: 'http://img.youtube.com/vi/' + id + '/default.jpg',
+      width: 120,
+      height: 90
+    },
+    medium: {
+      url: 'http://img.youtube.com/vi/' + id + '/mqdefault.jpg',
+      width: 320,
+      height: 180
+    },
+    high: {
+      url: 'http://img.youtube.com/vi/' + id + '/hqdefault.jpg',
+      width: 480,
+      height: 360
+    },
+  }
+};
